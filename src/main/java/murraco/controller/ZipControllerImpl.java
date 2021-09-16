828172b9777a1443d67a897ec1b110715afb54b4
Arman <arman_arshakyan@procode.am>
2021-09-16 14:13:44 +0400

    Zip codes.

--- src/main/java/murraco/controller/ZipControllerImpl.java
+++ src/main/java/murraco/controller/ZipControllerImpl.java
@@ -1,15 +1,24 @@
 package murraco.controller;
 
 import io.swagger.annotations.Api;
+import murraco.JwtAuthServiceApp;
 import murraco.model.ZipCodesModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 @RestController
@@ -17,18 +26,44 @@ import java.util.stream.Collectors;
 @Api(tags = "ZIP CODES")
 public class ZipControllerImpl {
 
-    private static final List<String> PARSED_ZIP_CODES = Arrays.asList(ZipController.FULL_ZIP_CODES.split(","));
+    private static List<String> PARSED_ZIP_CODES = new ArrayList<>();
 
 
-    @PostMapping("/zip")
+    @Value("classpath:output.xml")
+    Resource schemaFile;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+
+    @GetMapping("/zip-codes")
     public ResponseEntity<ZipCodesModel> getZipCodes(@RequestParam(required = true) String code) {
 
         ZipCodesModel zipCodesModel = new ZipCodesModel();
-        List<String> collect = PARSED_ZIP_CODES.stream().filter(zip -> zip.startsWith(code)).collect(Collectors.toList());
+        List<String> collect = getParsedZipCodes().stream().filter(zip -> zip.startsWith(code)).collect(Collectors.toList());
 
         zipCodesModel.setCodes(collect);
         return ResponseEntity.ok(zipCodesModel);
     }
 
+    public List<String> getParsedZipCodes() {
+        if (PARSED_ZIP_CODES.isEmpty()) {
+            Resource resource = resourceLoader.getResource("classpath:output.txt");
+            InputStream inputStream = null;
+            try {
+                inputStream = resource.getInputStream();
+                Scanner scanner = new Scanner(inputStream);
 
+                String str;
+                while ((str = scanner.nextLine()) != null) {
+                    PARSED_ZIP_CODES.add(str);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        return PARSED_ZIP_CODES;
+    }
 }
